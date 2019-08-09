46cb060bb96d315962d0b8af8a6c87c604c5730d
michaelbravo777 <michael.bravo777@gmail.com>
2019-08-09 09:25:19 -0400

    adding heroku

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ module.exports = {
     || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-  PROD_MIGRATION_DB_HOST: process.env.PROD_MIGRATION_DB_HOST || 'ec2-107-20-198-176.compute-1.amazonaws.com',
+  PROD_MIGRATION_DB_HOST: process.env.PROD_MIGRATION_DB_HOST || 'ec2-107-22-222-161.compute-1.amazonaws.com',
   PROD_MIGRATION_DB_PORT: process.env.PROD_MIGRATION_DB_PORT || 5432,
-  PROD_MIGRATION_DB_NAME: process.env.PROD_MIGRATION_DB_NAME || 'deia8raf4e0v97',
-  PROD_MIGRATION_DB_USER: process.env.PROD_MIGRATION_DB_USER || 'yhuedcupaiugrr',
-  PROD_MIGRATION_DB_PASS: process.env.PROD_MIGRATION_DB_PASS || '18315eeb5eee30ee80287e15420fcc6334cb10e1802cf5dcf4a6f8b258e30942',
+  PROD_MIGRATION_DB_NAME: process.env.PROD_MIGRATION_DB_NAME || 'dffvhktdm689ca',
+  PROD_MIGRATION_DB_USER: process.env.PROD_MIGRATION_DB_USER || 'bmmxmdapfqvgmw',
+  PROD_MIGRATION_DB_PASS: process.env.PROD_MIGRATION_DB_PASS || '362835a76112a20a4958a0b12a65352002a06aacbc0c67669ec277cd55baee4d',
 }
